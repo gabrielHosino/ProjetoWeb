f5cc79e894a55a87c4b7c0de99e97f9e880a442c
Majestoso Lucas <lucas.peres.nunes@gmail.com>
2016-06-12 16:56:57 -0300

    js dos contraladores

---
+++ ProjetoWeb/assets/js/angular/home.js
@@ -0,0 +1,27 @@
+var myApp = angular.module('inicial');
+
+//Service
+myApp.controller('posts', ['$scope', 'inicialService', function($scope, inicialService) {
+	var id;
+	var splitHref = location.href.split('=');
+	id = splitHref[1];
+	var user;
+	inicialService.user(id).then(
+		function(response){
+			inicialService.setUser(response.data[0]);
+			user = inicialService.getUser();
+			document.getElementsByTagName("uname")[0].innerHTML = user.firstname + " " + user.lastname;
+		},
+		//Error
+		function(response){
+			console.log('Erro: Problema no acesso ao banco de dados.');
+	});
+
+	document.getElementById("home").href = document.getElementById("home").href + ":id=" + id;
+	document.getElementById("profile").href = document.getElementById("profile").href + ":id=" + id;
+	document.getElementById("friends").href = document.getElementById("friends").href + ":id=" + id;
+	document.getElementById("groups").href = document.getElementById("groups").href + ":id=" + id;
+	document.getElementById("about").href = document.getElementById("about").href + ":id=" + id;
+	document.getElementById("contact").href = document.getElementById("contact").href + ":id=" + id;
+
+}]);
