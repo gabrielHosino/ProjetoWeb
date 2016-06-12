a01c7c1721e259292c270bb659148ade08807149
Majestoso Lucas <lucas.peres.nunes@gmail.com>
2016-06-12 18:59:28 -0300

    Atualiza perfil

--- ProjetoWeb/assets/js/angular/profile.js
+++ ProjetoWeb/assets/js/angular/profile.js
@@ -32,14 +32,67 @@ myApp.controller('profile', ['$scope', 'inicialService', function($scope, inicia
 	};
 
 	updateBio = function(){
-		var newdesc = document.getElementById("newbio");
+		var newdesc = document.getElementById("newbio").value;
+		var newfirstname = document.getElementById("newfirstname").value;
+		var newlastname = document.getElementById("newlastname").value;
+		var newdate = document.getElementById("newdate").value;
+		var confirm = document.getElementById("confirmEdit");
+		var newbio = document.getElementById("newbio");
+		var newfn = document.getElementById("newfirstname");
+		var newln = document.getElementById("newlastname");
+		var newdt = document.getElementById("newdate");
+		var bio = document.getElementsByTagName("udesc")[0];
+		var name = document.getElementsByTagName("uname")[0];
+		var date = document.getElementsByTagName("ubirth2")[0];
 
-		console.log(newdesc);
+		if(newdate != ""){
+			var dates = newdate.split("T");
+			date.innerHTML = dates[0];
+			//inicialService.updateBirth(dates[0]);
+		}else{
+			date.removeChild(newdt);
+		}
+
+		console.log("ON CHANGE");
+		console.log("NAME: " + newfirstname + " " + newlastname);
+		console.log("DATE: " + newdate);
+		console.log("DESC: " + newdesc);
+
+		if(newdesc != ""){
+			bio.innerHTML = newdesc;
+			//inicialService.updateBio(newdesc);
+		}
+		else{
+			bio.removeChild(confirm);
+			bio.removeChild(newbio);
+		}
+
+		if(newfirstname != ""){
+			name.innerHTML = newfirstname + " " + newlastname;
+			//inicialService.updateFirstname(newfirstname);
+			//inicialService.updateLastname(newlastname);
+		}
+		else{
+			name.removeChild(newfn);
+			name.removeChild(newln);
+		}
+
+		name.removeChild(newfn);
+		name.removeChild(newln);
+		date.removeChild(newdt);
+		bio.removeChild(confirm);
+		bio.removeChild(newbio);
 	};
 
 	editProfile = function(){
+		var names = document.getElementsByTagName("uname")[0];
+		var date = document.getElementsByTagName("ubirth2")[0];
 		var bio = document.getElementsByTagName("udesc")[0];
 		var newbio = document.createElement("INPUT");
+		var newfirstname = document.createElement("INPUT");
+		var newlastname = document.createElement("INPUT");
+		var newdate = document.createElement("INPUT");
+		newdate.setAttribute("type", "date");
 		var confirm = document.createElement("BUTTON");
 
 		confirm.id = "confirmEdit";
@@ -48,8 +101,19 @@ myApp.controller('profile', ['$scope', 'inicialService', function($scope, inicia
 		confirm.addEventListener("click", updateBio);
 
 		newbio.id = "newbio";
+		
+		newfirstname.id = "newfirstname";
+		newlastname.id = "newlastname";
+
+		newdate.id = "newdate";
+
 		bio.appendChild(newbio);
 		bio.appendChild(confirm);
+		date.appendChild(newdate);
+		names.appendChild(newfirstname);
+		names.appendChild(newlastname);
+
+		
 	};
 
 	showEdit = function(){
